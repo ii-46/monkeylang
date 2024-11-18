7f3ba8ed2ffc5b5f900501ab0d229e74ff07a008
Inthava <coci8750@gmail.com>
2024-11-18 19:44:39 +0700

    feat(token): add token

---
+++ token/token.go
@@ -0,0 +1,27 @@
+package token
+
+type Token struct {
+	Type    string
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foobar, x, y, ...
+	INT   = "INT"   // 1343456
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
