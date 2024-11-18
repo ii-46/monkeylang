5aa53a7dd627297611f9e75cc318e86faf13f8ed
Inthava <coci8750@gmail.com>
2024-11-18 20:06:52 +0700

    feat(lexer): add lexer struct and some helper function

---
+++ lexer/lexer.go
@@ -0,0 +1,27 @@
+package lexer
+
+type Lexer struct {
+	input        string
+	position     int  // current position in input (points to current char)
+	readPosition int  // current reading position in input (after current char)
+	ch           byte // current char under examination (corresponding to position)
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{
+		input: input,
+	}
+	l.readChar()
+	return l
+}
+
+// l.position always point to the last read charater
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.position
+	l.readPosition++
+}
