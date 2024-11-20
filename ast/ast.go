7374931769908c3ed38cb1ab77904aacc1db635d
Inthava <coci8750@gmail.com>
2024-11-20 17:01:00 +0700

    feat(ast): add String method

--- ast/ast.go
+++ ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/ii-46/monkeylang/token"
+import (
+	"bytes"
+
+	"github.com/ii-46/monkeylang/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -29,10 +34,20 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token // token.INDENT
 	Value string
 }
+
+func (i *Identifier) String() string { return i.Value }
 
 func (i *Identifier) TokenLiteral() string {
 	return i.Token.Literal
@@ -53,10 +68,21 @@ func (l *LetStatement) TokenLiteral() string {
 
 func (l *LetStatement) statementNode() {
 }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 type ReturnStatement struct {
-	Token token.Token
-	Value Expression
+	Token       token.Token
+	ReturnValue Expression
 }
 
 func (r *ReturnStatement) TokenLiteral() string {
@@ -64,6 +90,16 @@ func (r *ReturnStatement) TokenLiteral() string {
 }
 
 func (r *ReturnStatement) statementNode() {
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
 }
 
 type ExpressionStatement struct {
