605c6564019d5d17349ec60f74775e3ce1705001
Inthava <coci8750@gmail.com>
2024-11-20 13:40:46 +0700

    feat: add ast root node

---
+++ ast/ast.go
@@ -0,0 +1,28 @@
+package ast
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// This going to be root node of AST
+type Program struct {
+	Statements []Statement // Contain all statements
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
