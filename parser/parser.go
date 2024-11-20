ebbe10e87e01248cda92efb51baaa38988218b0c
Inthava <coci8750@gmail.com>
2024-11-20 15:11:20 +0700

    feat(parser): add parser error

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/ii-46/monkeylang/ast"
 	"github.com/ii-46/monkeylang/lexer"
 	"github.com/ii-46/monkeylang/token"
@@ -11,10 +13,25 @@ type Parser struct {
 
 	curToken  token.Token
 	peekToken token.Token
+
+	errors []string
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 	p.nextToken()
 	p.nextToken()
 	return p
